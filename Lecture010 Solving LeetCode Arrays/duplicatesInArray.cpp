3229edab56fb53fab53461be3c73efe18243d091
Vishnu-vashishth <91862297+Vishnu-vashishth@users.noreply.github.com>
2022-01-15 14:53:58 +0530

    Update duplicatesInArray.cpp

--- Lecture010 Solving LeetCode Arrays/duplicatesInArray.cpp
+++ Lecture010 Solving LeetCode Arrays/duplicatesInArray.cpp
@@ -4,14 +4,13 @@ int findDuplicate(vector<int> &arr)
 {
     int ans = 0;
     
-    //XOR ing all array elements
-    for(int i = 0; i<arr.size(); i++ ) {
-    	ans = ans^arr[i];
+    //XOR ing all array elements along with XOR [1, n-1]
+   for (int i=0;i<arr.size();i++){
+        ans = ans^arr[i];
+        ans = ans^i;
     }
 	
-    //XOR [1, n-1]
-    for(int i = 1; i<arr.size();i++ ) {
-    	ans = ans^i;
-    }
+  
+   
     return ans;
 }
